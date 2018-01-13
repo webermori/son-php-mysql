f3f2d71901e94f25d3b9bf4e19610d71b234516b
Weber Mori <webermori@gmail.com>
2018-01-13 11:35:29 -0200

    fetch _all

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php 	
-
 $server = "localhost";
 $user = "root";
 $pass = "";
@@ -9,18 +8,33 @@ $database = "son-php-mysql";
 @$mysqli = new mysqli($server, $user, $pass, $database);
 
 //Erro
-
 if (mysqli_connect_errno()) {
 	echo "Failded to connect to MySQL: (".$mysqli->connect_errno.") ".$mysqli->connect_error;
 	exit;
 }
-
 $sql = "SELECT * FROM USERS";
-$query = $mysqli->query($sql);
 
-while ($data = $query->fetch_assoc()) {
-	echo "ID: ".$data['id']."<br>";
-	echo "Name: ".$data['name']."<br>";
-	echo "E-mail: ".$data['email']."<hr>";
+//Getting Data
+//$query = $mysqli->query($sql);
+// while ($data = $query->fetch_assoc()) {
+// 	echo "ID: ".$data['id']."<br>";
+// 	echo "Name: ".$data['name']."<br>";
+// 	echo "E-mail: ".$data['email']."<hr>";
+// }
+
+if ($query = $mysqli->query($sql)) {
+	$user = $query->fetch_all(MYSQLI_BOTH);
+/*
+	fetch_all
+		MYSQLI_NUM
+		MYSQLI_ASSOC
+		MYSQLI_BOTH
+
+*/
+	foreach ($user as $value) {
+		echo $value[0]." - ".$value["name"]." - ".$value["email"] ."<br>";
+	}
+	//echo "ID: ".$user[0]["name"];
+
+	//print_r($user);
 }
-
