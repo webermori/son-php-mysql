ae8174a5ec675b187c12a20076639c664caaa2e9
Weber Mori <webermori@gmail.com>
2018-01-13 11:13:18 -0200

    Consumindo dados do banco

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $server = "localhost";
 $user = "root";
 $pass = "";
-$database = "";
+$database = "son-php-mysql";
 
 //Conecta o MySQL
 @$mysqli = new mysqli($server, $user, $pass, $database);
@@ -14,3 +14,13 @@ if (mysqli_connect_errno()) {
 	echo "Failded to connect to MySQL: (".$mysqli->connect_errno.") ".$mysqli->connect_error;
 	exit;
 }
+
+$sql = "SELECT * FROM USERS";
+$query = $mysqli->query($sql);
+
+while ($data = $query->fetch_assoc()) {
+	echo "ID: ".$data['id']."<br>";
+	echo "Name: ".$data['name']."<br>";
+	echo "E-mail: ".$data['email']."<hr>";
+}
+
